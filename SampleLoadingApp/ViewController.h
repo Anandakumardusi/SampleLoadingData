fc6b70754b52561d07d4e8e3b0ee62d45408094f
Paradigm Creatives <info@paradigmcreatives.com>
2016-05-24 13:50:06 +0530

    Initial Commit

---
+++ SampleLoadingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleLoadingApp
+//
+//  Created by Anand on 24/05/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleLoadingApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleLoadingApp
+//
+//  Created by Anand on 24/05/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
